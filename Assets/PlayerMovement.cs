2fbdd3a52a9108437a899023efdc0a5b2492e512
Muhammad Bilal <m.bilal9862@gmail.com>
2022-06-30 15:28:01 +0300

    ss

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,6 +7,12 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     public CharacterController controller;
     public float speed = 20.0f;
+
+    public float gravity = -9.81f;
+    Vector3 velocity;
+    public Transform groundcheck;
+    public LayerMask ground;
+    public float GroundDistance = 0.4;
     void Start()
     {
         
@@ -19,9 +25,21 @@ public class PlayerMovement : MonoBehaviour
         float Z = Input.GetAxis("Vertical");
 
 
+        if(velocity.y <0)
+        {
+            velocity.y = -2f;
+        }
+
         Vector3 move = transform.right * X + transform.forward * Z; //forward is dealt by Z
 
-
         controller.Move(move * speed * Time.deltaTime);
+
+
+
+        velocity.y += gravity * -2f* Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+
     }
 }
