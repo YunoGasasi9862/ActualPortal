7791c809f93cd0c6c1870cd1e9de6eb23a385a18
Muhammad Bilal <m.bilal9862@gmail.com>
2022-07-07 16:04:49 +0300

    ss

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -23,6 +23,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //float X = Input.GetAxis("Horizontal");
+        //float Z = Input.GetAxis("Vertical");
         float X = Input.GetAxis("Horizontal");
         float Z = Input.GetAxis("Vertical");
 
@@ -33,9 +35,12 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = -2f;
         }
 
-        Vector3 move = transform.right * X + transform.forward * Z; //forward is dealt by Z
-
+        Vector3 move = transform.right * X + transform.forward * Z;
         controller.Move(move * speed * Time.deltaTime);
+
+        //Vector3 move = transform.right * X + transform.forward * Z; //forward is dealt by Z
+
+        //controller.Move(move * speed * Time.deltaTime);
 
        if(Input.GetKey(KeyCode.Space))
         {
